aee25da206bd183d07f8dda14fe6f87772b65fdc
chiemelaonu <chiemelaonuoha2027@u.northwestern.edu>
2025-01-28 13:44:33 -0600

    splitting data

--- 1_initial_split.R
+++ 1_initial_split.R
@@ -7,3 +7,17 @@ library(tidymodels)
 
 # resolve conflicts
 tidymodels_prefer()
+
+
+# initial split ----
+movies_split <- movies |>
+  initial_split(prop = 0.8, strata = grossWW_log10)
+
+movies_training <- movies_split |> training()
+movies_testing <- movies_plit |> testing()
+
+
+# write out/save outputs ----
+save(movies_split, file = here::here("data/movies_split.rda"))
+save(movies_training, file = here::here("data/movies_training.rda"))
+save(movies_testing, file = here::here("data/movies_testing.rda"))
