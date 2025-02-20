e13281074ede3c442bca7d3e6b4f51bfcfecc2ad
chiemelaonu <chiemelaonuoha2027@u.northwestern.edu>
2025-02-19 22:36:17 -0600

    saving null recipe

--- 2_recipes.R
+++ 2_recipes.R
@@ -12,14 +12,24 @@ tidymodels_prefer()
 # load in training data ----
 load(here("data/movies_train.rda"))
 
-# build recipe ----
+# build null recipe ----
+null_recipe <- recipe(yeo_revenue ~ 1, movies_train)
+
+
+# null_recipe |>
+#   prep() |>
+#   bake(new_data = NULL) |> glimpse()
+#   
+
+# build primary recipe ----
 movies_recipe <- recipe(yeo_revenue ~ score + budget_x + date, movies_train) |>
   step_sqrt(budget_x) |>
   step_date(date, features = "year", keep_original_cols = FALSE)
 
-# five <- movies_recipe |>
+# movies_recipe |>
 #   prep() |>
 #   bake(new_data = NULL) |> glimpse()
 
 # save results ----
 save(movies_recipe, file = here("recipes/movies_recipe.rda"))
+save(null_recipe, file = here("recipes/null_recipe.rda"))
