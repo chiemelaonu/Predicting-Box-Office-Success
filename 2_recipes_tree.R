9feb04c6847cc627bdc1eb6c97bf047869680a33
chiemelaonu <chiemelaonuoha2027@u.northwestern.edu>
2025-03-02 12:39:05 -0600

    tree model recipe

--- 2_recipes_tree.R
+++ 2_recipes_tree.R
@@ -13,28 +13,18 @@ tidymodels_prefer()
 load(here("data/movies_train.rda"))
 
 # first OLS recipe ----
-movies_recipe_ols <- recipe(yeo_revenue ~ score + budget_x + date
-                        + negative + positive + overall_sentiment + num_crew + num_genres, data = movies_train) |>
+movies_recipe_tree <- recipe(yeo_revenue ~ score + budget_x + date
+                             + negative + positive + overall_sentiment + num_crew + num_genres, data = movies_train) |>
   step_impute_mean(positive, negative) |>
   step_impute_mode(overall_sentiment) |>
   step_date(date, features = c("year", "month"), keep_original_cols = FALSE) |>
-  step_interact(terms = ~ budget_x:num_crew ) |> 
   step_dummy(all_nominal_predictors(), one_hot = TRUE) |>
+  step_zv(all_predictors()) |> 
   step_normalize(all_numeric_predictors())
 
-# second OLS recipe (without interactions)
-# movies_recipe_ols_basic <- recipe(yeo_revenue ~ score + budget_x + date
-#                         + negative + positive + overall_sentiment + num_crew + num_genres, data = movies_train) |>
-#   step_impute_mean(positive, negative) |>
-#   step_impute_mode(overall_sentiment) |>
-#   step_date(date, features = c("year", "month"), keep_original_cols = FALSE) |>
-#   step_dummy(all_nominal_predictors(), one_hot = TRUE) |>
-#   step_normalize(all_numeric_predictors())
-
-# movies_recipe_ols |>
-#   prep() |>
-#   bake(new_data = NULL) |> glimpse()
+movies_recipe_tree |>
+  prep() |>
+  bake(new_data = NULL) |> glimpse()
 
 # save results ----
-# save(movies_recipe_ols_basic, file = here("recipes/movies_recipe_ols_basic.rda"))
-save(movies_recipe_ols, file = here("recipes/movies_recipe_ols.rda"))
+save(movies_recipe_tree, file = here("recipes/movies_recipe_tree.rda"))
