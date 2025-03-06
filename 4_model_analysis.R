dfc91faf5fa813f55eefde870bfda5e9ac4722b2
chiemelaonu <chiemelaonuoha2027@u.northwestern.edu>
2025-03-06 13:01:11 -0600

    basic and complex model metrics/analysis

--- 4_model_analysis.R
+++ 4_model_analysis.R
@@ -15,7 +15,7 @@ tidymodels_prefer()
 num_cores <- parallel::detectCores(logical = FALSE)
 registerDoMC(cores = 6)
 
-# load in training data ----
+# load in results ----
 list.files(
   here("results/"),
   pattern = ".rda",
@@ -24,7 +24,7 @@ list.files(
   map(load, envir = .GlobalEnv)
 
 # examine results
-model_results <-
+basic_model_results <-
   as_workflow_set(
     lm = lm_basic_fit,
     null = null_results,
@@ -33,6 +33,42 @@ model_results <-
     rf = rf_tuned_basic,
     bt = bt_tuned_basic,
     en = en_tuned_basic
+  ) 
+
+# examine rmse
+basic_model_results |>
+  autoplot(metric = "rmse")
+
+basic_model_results |>
+  autoplot(metric = "rmse", select_best = TRUE)
+
+basic_model_results |>
+  collect_metrics() 
+
+basic_fits_table <- basic_model_results |>
+  collect_metrics() |>
+  filter(.metric == "rmse") |>
+  slice_min(mean, by = wflow_id) |>
+  arrange(mean) |>
+  select(
+    `Model Name` = wflow_id,          
+    `Metric Type` = .metric,          
+    `Mean RMSE` = mean,               
+    `Standard Error` = std_err,
+    n              
+  ) |> knitr::kable()
+
+
+# examine results
+model_results <-
+  as_workflow_set(
+    lm = lm_fit,
+    null = null_results,
+    baseline = baseline_results,
+    knn = knn_tuned,
+    rf = rf_tuned,
+    bt = bt_tuned,
+    en = en_tuned
   ) 
 
 # examine rmse
@@ -45,7 +81,7 @@ model_results |>
 model_results |>
   collect_metrics() 
 
-basic_fits_table <- model_results |>
+fits_table <- model_results |>
   collect_metrics() |>
   filter(.metric == "rmse") |>
   slice_min(mean, by = wflow_id) |>
